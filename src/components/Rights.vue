9ff202e90ad69fce4a027b33c94e5d13b60025b9
156174460 <302969547@qq.com>
2019-10-28 10:21:29 +0800

    角色列表完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,13 +1,69 @@
 <template>
-  <div class="rights">rights组件</div>
+  <div class="roles">
+<!-- 面包屑 -->
+<el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+  <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+</el-breadcrumb>
+<hr>
+<!-- biaoge -->
+
+    <el-table
+      :data="rightsList"
+      >
+      <el-table-column type="index"></el-table-column>
+      <el-table-column
+        prop="authName"
+        label="权限名称"
+       >
+      </el-table-column>
+      <el-table-column
+        prop="path"
+        label="路径"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="层级">
+        <template v-slot:default="{row}">
+          <p v-if="row.level==='0'">一级</p>
+          <p v-if="row.level==='1'">二级</p>
+          <p v-if="row.level==='2'">三级</p>
+        </template>
+
+      </el-table-column>
+    </el-table>
+
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      rightsList: []
+    }
+  },
+  async created () {
+    const { data, meta } = await this.$axios.get('rights/list')
+    console.log(data, meta)
 
+    if (meta.status === 200) {
+      this.rightsList = data
+    } else {
+      this.$message.error(meta.msg)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+body {
+  background-color: #ecf0f1;
+}
+.el-breadcrumb {
+  height: 40px;
+line-height: 40px;
+}
 </style>
--- src/components/Roles.vue
+++ src/components/Roles.vue
@@ -1,13 +1,58 @@
 <template>
-  <div class="roles">roles组件</div>
+  <div class="roles">
+<!-- 面包屑 -->
+<el-breadcrumb separator-class="el-icon-arrow-right">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+  <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+</el-breadcrumb>
+<hr>
+<!-- biaoge -->
+  <template>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+  </template>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+    // rolesList=[]
+    }
+  },
+  created () {
 
+  },
+  methods () {
+
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+body {
+  background-color: #ecf0f1;
+}
+.el-breadcrumb {
+  height: 40px;
+line-height: 40px;
+}
 </style>
